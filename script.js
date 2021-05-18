ecabf37a6acd6fd2b51c4365655f4ec28c65c846
Ambeeee <77481329+Ambeeee@users.noreply.github.com>
2021-05-18 22:11:00 +0200

    Test Responsive

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 //Responsive
 
-var menu = "colse";
+var menu = "close";
 
 function MenuMobile(){
     var mobile_menu = document.getElementById("mobileNav");
+    var iconmenu = document.getElementById("menuIcon");
 
     if(menu == "close"){
         mobileNav.style.width = "100%";
-        menu="open";      
+        menu="open";
+        iconmenu.src = "img/Generale/IconaMenuChiudi.png"    
     }
 
     else{
         mobileNav.style.width = "0%";
         menu = "close"
-
+        iconmenu.src = "img/Generale/IconaMenu1.png" 
     }
 }
