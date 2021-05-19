25c478cd57ef642c329fd17304ed2183500f7077
Ambeeee <77481329+Ambeeee@users.noreply.github.com>
2021-05-19 08:55:06 +0200

    Responsive1.1

--- script.js
+++ script.js
@@ -3,18 +3,23 @@
 var menu = "close";
 
 function MenuMobile(){
-    var mobile_menu = document.getElementById("mobileNav");
+    var mobileNav = document.getElementById("mobileNav");
     var iconmenu = document.getElementById("menuIcon");
-
+    var intestazione = document.getElementById("intestazione")
+    var sfondo1 = document.getElementById("sfondo1")
     if(menu == "close"){
-        mobileNav.style.width = "100%";
+        mobileNav.style.display = "flex";
+        intestazione.style.display = "none";
+        sfondo1.style.display = "none";
         menu="open";
-        iconmenu.src = "img/Generale/IconaMenuChiudi.png"    
+        iconmenu.src = "img/Generale/IconaMenuChiudi.png";    
     }
 
     else{
-        mobileNav.style.width = "0%";
+        mobileNav.style.display = "none";
+        intestazione.style.display = "block";
+        sfondo1.style.display = "flex";
         menu = "close"
-        iconmenu.src = "img/Generale/IconaMenu1.png" 
+        iconmenu.src = "img/Generale/IconaMenu1.png"; 
     }
 }
