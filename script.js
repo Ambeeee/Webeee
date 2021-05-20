673c23aa072887f0fb74bcd73bb85b6cf5cf93f7
Ambeeee <77481329+Ambeeee@users.noreply.github.com>
2021-05-20 10:12:12 +0200

    Responsive News 1.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Responsive
+//Responsive Home
 
 var menu = "close";
 
@@ -20,6 +20,47 @@ function MenuMobile(){
         intestazione.style.display = "block";
         sfondo1.style.display = "flex";
         menu = "close"
-        iconmenu.src = "img/Generale/IconaMenu1.png"; 
+        iconmenu.src = "img/Generale/IconaMenu1.png" 
+    }
+}
+
+
+//Responsive News
+
+function MenuMobile1(){
+    var mobileNav = document.getElementById("mobileNav");
+    var iconmenu = document.getElementById("menuIcon");
+    var intestazione = document.getElementById("intestazione")
+    var sfondo1 = document.getElementById("sfondo1")
+    if(menu == "close"){
+        mobileNav.style.display = "flex";
+        intestazione.style.display = "none";
+        sfondo1.style.display = "none";
+        menu="open";
+        iconmenu.src = "../img/Generale/IconaMenuChiudi.png";    
+    }
+
+    else{
+        mobileNav.style.display = "none";
+        intestazione.style.display = "block";
+        sfondo1.style.display = "flex";
+        menu = "close"
+        iconmenu.src = "../img/Generale/IconaMenu1.png" 
+    }
+}
+
+//News Mobile
+function NewsMobile(){
+    var ModsMobile = document.getElementById("ModsMobile");
+    var pagina2 = document.getElementById("paginaMods2")
+    if(menu == "close"){
+        ModsMobile.style.display = "flex";
+        pagina2.style.display
+        menu="open";    
+    }
+
+    else{
+        ModsMobile.style.display = "none";
+        menu = "close";
     }
 }
