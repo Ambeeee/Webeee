6924e0e81dfb54704a0cf32d1c5465b6a339ce11
Ambeeee <77481329+Ambeeee@users.noreply.github.com>
2021-05-21 20:41:52 +0200

    Responsive Pres 1.0

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function MenuMobile(){
 
 //Responsive News
 
-function MenuMobile1(){
+function MenuMobileNews(){
     var mobileNav = document.getElementById("mobileNav");
     var iconmenu = document.getElementById("menuIcon");
     var intestazione = document.getElementById("intestazione")
@@ -62,5 +62,31 @@ function NewsMobile(){
     else{
         ModsMobile.style.display = "none";
         menu = "close";
+    }
+}
+
+//Pres Mobile
+function MenuMobilePres(){
+    var mobileNav = document.getElementById("mobileNav");
+    var iconmenu = document.getElementById("menuIcon");
+    var intestazione = document.getElementById("intestazione")
+    var sfondo1 = document.getElementById("sfondo1")
+    var intro = document.getElementById("introPresM")
+    if(menu == "close"){
+        mobileNav.style.display = "flex";
+        intestazione.style.display = "none";
+        sfondo1.style.display = "none";
+        intro.style.display = "none";
+        menu="open";
+        iconmenu.src = "../../img/Generale/IconaMenuChiudi.png";    
+    }
+
+    else{
+        mobileNav.style.display = "none";
+        intestazione.style.display = "block";
+        sfondo1.style.display = "flex";
+        intro.style.display = "flex";
+        menu = "close"
+        iconmenu.src = "../../img/Generale/IconaMenu1.png" 
     }
 }
